28b7dbe4f72788a8a5cc1b560359bf239c63b3c3
yulongtan <swifties@uw.edu>
2016-04-07 12:04:27 -0700

    removed else redundancy in type checks

--- src/Sorter.java
+++ src/Sorter.java
@@ -10,6 +10,11 @@ import java.util.Queue;
  */
 
 public class Sorter<E> {
+    private boolean less;
+
+    public Sorter() {
+        this.less = false;
+    }
 
     public void mergeSort(LinkedQueue<E> list) {
         if (list.size() > 1) {
@@ -35,24 +40,25 @@ public class Sorter<E> {
         while (!left.isEmpty() && !right.isEmpty()) {
             E leftData = left.peek();
             E rightData = right.peek();
+            this.less = false;
             if (this.isString(leftData) || this.isString(rightData)) {
                 if (((String) leftData).compareTo((String) rightData) <= 0) {
                     list.add(left.remove());
-                } else {
-                    list.add(right.remove());
+                    this.less = true;
                 }
             } else if (this.isInt(leftData) || this.isInt(rightData)) {
                 if (((Integer) leftData).compareTo((Integer) rightData) <= 0) {
                     list.add(left.remove());
-                } else {
-                    list.add(right.remove());
+                    this.less = true;
                 }
             } else {
                 if (((Double) leftData).compareTo((Double) rightData) <= 0) {
                     list.add(left.remove());
-                } else {
-                    list.add(right.remove());
+                    this.less = true;
                 }
+            }
+            if (!this.less) {
+                list.add(right.remove());
             }
         }
         while (!left.isEmpty()) {
